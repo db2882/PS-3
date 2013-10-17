c6a16228e1deb6622362f0249c764e77ee6c9353
David Bain <david@dnbain.com>
2013-10-17 13:55:03 -0400

    2nd commit

---
+++ BlueCommand.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Point;
+
+
+public class BlueCommand extends Command {
+	public void executeClick(Point p, Drawing dwg) { 
+		if(dwg.getFrontmostContainer(p)!=null){
+			dwg.getFrontmostContainer(p).setColor(Color.blue);
+		}
+	}
+}
